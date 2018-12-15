e88e140680fd39dc129387faa6a68b6519cd063f
Humberto Millan <humbertomillan92@gmail.com>
2018-12-14 21:11:24 -0400

    MVC

---
+++ ON4X.Domain/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace ON4X.Domain.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+    }
+}
