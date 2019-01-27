1a43aaa87136481b0103e809aa330a923b9801bb
Humberto Millan <humbertomillan92@gmail.com>
2019-01-27 18:29:22 -0400

    Tutorial 56/04
    video 4

---
+++ ON4X.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace ON4X.Backend.Models
+{
+    using Domain.Models;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<ON4X.Common.Models.Product> Products { get; set; }
+    }
+}
