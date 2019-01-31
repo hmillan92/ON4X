8a493dab3a80a69bb87a36eebd5c662c47c8ea5a
Humberto Millan <humbertomillan92@gmail.com>
2019-01-30 23:22:35 -0400

    Consumo de Servicios Rest Full

---
+++ ON4X.Common/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿namespace ON4X.Common.Models
+{
+    public class Response
+    {
+
+        public bool IsSuccess { get; set; }
+
+
+        public string Message { get; set; }
+
+ 
+        public object Result { get; set; }
+    }
+}
